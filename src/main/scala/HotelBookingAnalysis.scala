320986e22c4632bf332fcfb707052711fde53ee5
Guangrou Chua <guangrouchua@gmail.com>
2025-12-03 21:28:07 +0800

    Update HotelBookingAnalysis.scala
    update part of our code based on the clarified requirements for q2 and q3, halfway to done

--- src/main/scala/HotelBookingAnalysis.scala
+++ src/main/scala/HotelBookingAnalysis.scala
@@ -11,10 +11,18 @@ import java.io.FileReader
 case class Booking(
                   hotelName: String,
                   originCountry: String,
+                  destinationCountry: String,
                   bookingPrice: Double,
                   discount: Double,
-                  profitMargin: Double
+                  profitMargin: Double,
+                  noOfDays: Int,
+                  rooms: Int,
 ) {
+  def pricePerRoomPerDay: Double = {
+    val days = if (noOfDays <= 0) 1 else noOfDays
+    val roomCount = if (rooms <= 0) 1 else rooms
+    bookingPrice / (days * roomCount)
+  }
   def netCustomerCost: Double = bookingPrice * (1.0 - discount)
   def profitPerBooking: Double = bookingPrice * profitMargin
 }
@@ -23,6 +31,8 @@ object Utils {
   def safeToDouble(str: String): Double =
     try str.toDouble catch { case _: Throwable => 0.0}
 
+  def safeToInt(str: String): Int =
+    try str.toInt catch {case _: Throwable => 1}
   def safeParseDiscount(discountStr: String): Double = {
     try {
       discountStr.replace("%", "").trim.toDouble / 100.0
@@ -38,13 +48,12 @@ class HotelDataProcessor(filePath: String) {
 
   import Utils._
 
-  private val processedData: List[Booking] = loadData()
+  private lazy val processedData: List[Booking] = loadData()
 
   def getAllBookings: List[Booking] = processedData
 
   private def loadData(): List[Booking] = {
     val file = new File(filePath)
-
     if (!file.exists()) {
       println(s"CRITICAL ERROR: File not found at path: ${file.getAbsolutePath}")
       return List.empty[Booking]
@@ -55,26 +64,28 @@ class HotelDataProcessor(filePath: String) {
     var reader: BufferedReader = null
     try {
       reader = new BufferedReader(new FileReader(file))
-
       val lines = Iterator.continually(reader.readLine()).takeWhile(_ != null).drop(1).toList
 
       if (lines.isEmpty) {
         println("CRITICAL ERROR: File is empty or reading failed after finding file.")
         return List.empty[Booking]
       }
+
       lines.flatMap { line =>
         val cols = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)").map(_.trim)
-
         if (cols.length >= 24) {
           try {
             val hotel = cols(16).replaceAll("\"", "")
-            val country = cols(6).replaceAll("\"", "")
+            val countryOrigin = cols(6).replaceAll("\"", "")
+            val countryDestination = cols(9).replaceAll("\"","")
             val price = safeToDouble(cols(20))
             val discountRatio = safeParseDiscount(cols(21))
             val profit = safeToDouble(cols(23))
+            val days = safeToInt(cols(13))
+            val rooms = safeToInt(cols(15))
 
-            if (country.nonEmpty && price > 0)
-              Some(Booking(hotel, country, price, discountRatio, profit))
+            if (countryOrigin.nonEmpty && price > 0)
+              Some(Booking(hotel, countryOrigin,countryDestination, price, discountRatio, profit, days, rooms))
             else
               None
           } catch {
@@ -94,7 +105,46 @@ class HotelDataProcessor(filePath: String) {
   }
 }
 
-//3. Polymorphism: Analysis Traits and Implementations
+//3. Polymorphism & Scoring Logic
+case class Q2GlobalBounds(
+  priceMin: Double, priceMax: Double,
+  discountMin: Double, discountMax: Double,
+  profitMin: Double, profitMax: Double
+)
+
+case class Q3GlobalBounds(
+  visitorsMin: Double, visitorsMax: Double,
+  avgProfitMin: Double, avgProfitMax: Double
+)
+
+object ScoringEngine {
+  def normalize(value: Double, min: Double, max: Double): Double={
+    if(max-min==0) 0.0
+    else(value-min) / (max-min) * 100
+  }
+
+  def calculateQ2Bounds(bookings: List[Booking]):Q2GlobalBounds={
+    val prices = bookings.map(_.pricePerRoomPerDay)
+    val discounts = bookings.map(_.discount)
+    val profits = bookings.map(_.profitMargin)
+
+    Q2GlobalBounds(
+      priceMin = prices.min, priceMax = prices.max,
+      discountMin = discounts.min, discountMax = discounts.max,
+      profitMin = profits.min, profitMax = profits.max
+    )
+  }
+
+  def calculateQ3Bounds(hotelMetrics: Map[String, (Int, Double)]): Q3GlobalBounds={
+    val visitorCounts = hotelMetrics.values.map(_._1.toDouble)
+    val avgProfits = hotelMetrics.values.map(_._2)
+
+    Q3GlobalBounds(
+      visitorsMin = visitorCounts.min, visitorsMax = visitorCounts.max,
+      avgProfitMin = avgProfits.min, avgProfitMax = avgProfits.max
+    )
+  }
+}
 
 trait AnalysisReport {
   def analyze(bookings: List[Booking]): Unit
@@ -104,7 +154,7 @@ trait AnalysisReport {
 class CountryAnalysis extends AnalysisReport {
   def analyze(bookings: List[Booking]): Unit = {
     val result = bookings
-      .groupBy(_.originCountry)
+      .groupBy(_.destinationCountry)
       .mapValues(_.size)
       .maxByOption { case (_, count) => count }
     
@@ -123,8 +173,8 @@ class EconomicalAnalysis extends AnalysisReport{
   def analyze(bookings: List[Booking]):Unit={
     val result = bookings
       .groupBy(_.hotelName)
-      .mapValues(list => list.map(_.netCustomerCost).min)
-      .minByOption {case(_,minNetCost)=> minNetCost}
+      .mapValues(list => list.map(_.bookingPrice).sum/list.size)
+      .minByOption {case(_,minAvgCost)=> minAvgCost}
 
     println("\n[2. Most Economical Hotel Analysis]")
     result match{
